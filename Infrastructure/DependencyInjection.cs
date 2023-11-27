a5a82b4a37cd2f4f8fa68077037029831bd8bdff
Markuslarsson95 <markus.larsson@student.nbi-handelsakademin.se>
2023-11-27 11:22:12 +0100

    db started

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using Infrastructure.Database;
+using Infrastructure.RealDatabase;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
@@ -8,6 +10,11 @@ namespace Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<MockDatabase>();
+            services.AddDbContext<MySqlDB>(options =>
+            {
+                options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
+                //options.UseSqlServer("SERVER=localhost;DATABASE=animals;UID=root;PASSWORD=1234;");
+            });
             return services;
         }
     }
---
+++ Infrastructure/RealDatabase/MySqlDB.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.RealDatabase
+{
+    public class MySqlDB : DbContext
+    {
+        public MySqlDB() { }
+
+        public MySqlDB(DbContextOptions<MySqlDB>options) : base(options) { }
+
+        public DbSet<Dog> Dogs { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database = animals; Uid = root; Pwd = 1234;");
+        //    //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Animals;Uid=root;Pwd=1234;");
+        //}
+    }
+}
