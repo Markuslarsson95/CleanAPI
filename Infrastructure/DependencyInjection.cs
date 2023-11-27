1de22efc1b168706c01b390260d6465e4c51f628
Markuslarsson95 <markus.larsson@student.nbi-handelsakademin.se>
2023-11-27 11:50:39 +0100

    not done

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -12,8 +12,8 @@ namespace Infrastructure
             services.AddSingleton<MockDatabase>();
             services.AddDbContext<MySqlDB>(options =>
             {
-                options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
-                //options.UseSqlServer("SERVER=localhost;DATABASE=animals;UID=root;PASSWORD=1234;");
+                //options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=animals;Uid=root;Pwd=1234;");
+                options.UseSqlServer("server=127.0.0.1\\mssqllocaldb;uid=root;pwd=1234;database=animals");
             });
             return services;
         }
